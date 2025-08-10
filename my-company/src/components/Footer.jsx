1004714c6fd91152431825fe9dcf735218ffd89f
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-08-10 21:08:02 +0300

    fix: move components folder into src

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+    return (
+    <footer style={{backgroundColor: '#333',
+                    color: 'white',
+                    padding: '10px',
+                    textAlign: 'center'
+    }}>
+    <p>© 2025 digionn. All rights reserved.</p>
+    </footer>
+);
+}
+export default Footer;
