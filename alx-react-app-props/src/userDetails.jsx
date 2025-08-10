5827d3a9b08d775a50095b5059ed22d165c51555
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-08-10 07:59:36 +0300

    fix: code

--- alx-react-app-props/src/userDetails.jsx
+++ alx-react-app-props/src/userDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { UserContext } from './context/UserContext';
+import { UserContext } from './components/UserContext';
 
 function UserDetails() {
     const userData = useContext(UserContext);
