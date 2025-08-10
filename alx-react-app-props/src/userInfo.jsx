aba6185ea78098f115ce8564c1e3c6f5dd650fa8
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-08-10 06:00:54 +0300

    fix: rename userContext.js to UserContext.js

--- alx-react-app-props/src/userInfo.jsx
+++ alx-react-app-props/src/userInfo.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import UserContext from './context/userContext';
+import UserContext from './context/UserContext';
 
 function UserInfo() {
     const userData = useContext(UserContext);
