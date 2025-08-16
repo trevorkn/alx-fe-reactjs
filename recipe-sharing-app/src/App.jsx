0304e7675492bbf47b82c96147c9118b2c48af3d
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-08-16 16:11:24 +0300

    feat: Initial commit!

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import RecipeList from './components/RecipeList';
+
+import AddRecipeForm from './components/AddRecipeForm';
+
+function App() {
+
+  return (
+    <>
+    <h1>Recipe Sharing App</h1>
+     <RecipeList />
+     <AddRecipeForm />
+    </>
+  )
+}
+
+export default App
