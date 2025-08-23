20c8ca4f62dffe89928fc3bc5f01c7064d93bb47
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-08-24 00:35:33 +0300

    Add: add hover effects on the image card and header

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
     return (
-        <div className="bg-gray-100 sm:p-4 md:p-8 md:max-w-sm sm:max-w-xs mx-auto,my-20 rounded-lg shadow-lg" >
-            <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto" />
-            <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
-            <p className="sm:text-sm md:text-base text-gray-600 text-base">Developer at Example Co. loves to write code and explore new technologies.</p>
+        <div className="bg-gray-100 sm:p-4 md:p-8 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl" >
+            <img src="/jonDoe.jpeg" alt="User" className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" />
+            <h1 className="md:text-xl sm:text-lg text-blue-800 my-4 hover:text-blue-500">John Doe</h1>
+            <p className="sm:text-sm md:text-base text-gray-600">Developer at Example Co. loves to write code and explore new technologies.</p>
             
         </div>
     );
