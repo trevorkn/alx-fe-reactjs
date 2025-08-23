0e064edc85fbb7f4400d5bcbf96fa1cabb9e7322
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-08-24 00:07:32 +0300

    feat: adding responsive typography to UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
     return (
-        <div className="bg-gray-100 p-8 max-w-sm mx-auto,my-20 rounded-lg shadow-lg" >
-            <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto" />
-            <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-            <p className="text-gray-600 text-base">Developer at Example Co. loves to write code and explore new technologies.</p>
+        <div className="bg-gray-100 sm:p-4 md:p-8 max-w-sm max-w-sm mx-auto,my-20 rounded-lg shadow-lg" >
+            <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto" />
+            <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+            <p className="sm:text-sm md:text-base text-gray-600 text-base">Developer at Example Co. loves to write code and explore new technologies.</p>
             
         </div>
     );
