f07e839655477e47d5acd32b4400552f1644f901
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-08-23 23:48:15 +0300

    fix: the tailwind css in img heading from h36 to h-36

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
     return (
         <div className="bg-gray-100 p-8 max-w-sm mx-auto,my-20 rounded-lg shadow-lg" >
-            <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h36 mx-auto" />
+            <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto" />
             <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
             <p className="text-gray-600 text-base">Developer at Example Co. loves to write code and explore new technologies.</p>
             
