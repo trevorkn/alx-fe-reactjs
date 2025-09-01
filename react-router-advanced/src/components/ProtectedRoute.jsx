19e145eaff66b8ad1219dcf67c3a727b481695be
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-09-01 18:32:17 +0300

    feat(auth): intergrate useAuth hook into protectedRoute for authentication

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
 import { Navigate } from "react-router-dom";
-
-const isAuthenticated = false; // true to allow access
+import useAuth from "../hooks/useAuth";
 
 export default function ProtectedRoute({ children }) {
-    if(!isAuthenticated){
+    const { isAuthenticated } = useAuth();
+
+    if(!isAuthenticated) {
         return <Navigate to="/login" replace />;
     }
     return children;
