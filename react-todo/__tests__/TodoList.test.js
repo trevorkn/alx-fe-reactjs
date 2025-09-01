609534947bfae16701afce2ae386cf904214d03a
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-09-01 19:56:50 +0300

    configure jest with Babel to support jsx in TodoList tests

--- react-todo/__tests__/TodoList.test.js
+++ react-todo/__tests__/TodoList.test.js
@@ -1,9 +1,9 @@
+import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import TodoList from "../src/components/TodoList"; // adjust path if needed
+import TodoList from "../src/components/TodoList";
 import "@testing-library/jest-dom";
 
 describe("TodoList Component", () => {
-  // Initial render test
   test("renders initial todos", () => {
     render(<TodoList />);
     expect(screen.getByText("Learn React")).toBeInTheDocument();
@@ -11,38 +11,34 @@ describe("TodoList Component", () => {
     expect(screen.getByText("Write Tests")).toBeInTheDocument();
   });
 
-  // Adding todos
   test("can add a new todo", () => {
     render(<TodoList />);
     const input = screen.getByPlaceholderText("Add a new todo...");
-    const addButton = screen.getByText("Add");
+    const button = screen.getByText("Add");
 
     fireEvent.change(input, { target: { value: "New Task" } });
-    fireEvent.click(addButton);
+    fireEvent.click(button);
 
     expect(screen.getByText("New Task")).toBeInTheDocument();
   });
 
-  // Toggling todos
   test("can toggle a todo completion", () => {
     render(<TodoList />);
     const todo = screen.getByText("Learn React");
-    expect(todo).not.toHaveClass("line-through");
 
+    expect(todo).not.toHaveClass("line-through");
     fireEvent.click(todo);
     expect(todo).toHaveClass("line-through");
-
     fireEvent.click(todo);
     expect(todo).not.toHaveClass("line-through");
   });
 
-  // Deleting todos
   test("can delete a todo", () => {
     render(<TodoList />);
     const todo = screen.getByText("Learn React");
-    const deleteButton = todo.nextSibling; // Delete button
-    fireEvent.click(deleteButton);
+    const deleteButton = todo.nextSibling;
 
+    fireEvent.click(deleteButton);
     expect(todo).not.toBeInTheDocument();
   });
 });
